5903d51a2fe25310b0b8b975d487daeb2152f1f6
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-06-22 22:54:30 +0300

    some modif

--- resources/views/components/ui/menu.blade.php
+++ resources/views/components/ui/menu.blade.php
@@ -1,17 +1,17 @@
-<menu class="fixed w-full top-0 left-0 p-6 bg-red-100 flex justify-between shadow-lg">
+<menu class="fixed w-full top-0 left-0 py-6 pl-6 pr-12 bg-red-100 flex justify-between items-center">
     <div>
-        <a href="">Logo</a>
+        <a href="" class=" font-semibold text-3xl">SmartLens</a>
     </div>
     <nav>
         <ul class="flex gap-4">
-            <li><a href="">Home</a></li>
+            <li><a href="">home</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Services</a></li>
             <li><a href="">Contact</a></li>
         </ul>
     </nav>
-    <div class="flex gap-4">
-        <a href="">Register</a>
-        <a href="">Login</a>
+    <div class="flex gap-2">
+        <a href="" class="p-2 bg-blue-300 rounded">register</a>
+        <a href="" class="p-2 bg-blue-300 rounded">login</a>
     </div>
 </menu>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-    TEST
+    <section class="herosection">
+        Herosection
+    </section>
+    <section class="about">
+        About
+    </section>
+    <section class="service">
+        Services
+    </section>
 </x-layout>
